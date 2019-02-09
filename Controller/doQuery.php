0055569156d8c9d595308247d6fd5b83c034f078
william <william_1188@yahoo.com>
2019-02-09 20:18:12 +0700

    Initial commit

--- Controller/doQuery.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require("../Data/db.php");
-    $query = $_POST['query'];
-    $statement = $db->prepare($query);
-    $statement->execute();
-    
-    $statement->close();
-    echo 'success!';
